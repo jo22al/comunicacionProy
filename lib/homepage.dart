9fb5c6316e68d4603524690f1e6b36c669f2d25d
jo22al <joeltaguit@gmail.com>
2023-10-13 22:24:49 -0600

    Commit inicial

---
+++ lib/homepage.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+import 'flipcardgame.dart';
+import 'data.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: _list.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => _list[index].goto,
+                    ));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: _list[index].primarycolor,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 4,
+                                color: Colors.black45,
+                                spreadRadius: 0.5,
+                                offset: Offset(3, 4))
+                          ]),
+                    ),
+                    Container(
+                      height: 90,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: _list[index].secomdarycolor,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 4,
+                                color: Colors.black12,
+                                spreadRadius: 0.3,
+                                offset: Offset(
+                                  5,
+                                  3,
+                                ))
+                          ]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                              child: Text(
+                            _list[index].name,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                shadows: [
+                                  Shadow(
+                                    color: Colors.black26,
+                                    blurRadius: 2,
+                                    offset: Offset(1, 2),
+                                  ),
+                                  Shadow(
+                                      color: Colors.green,
+                                      blurRadius: 2,
+                                      offset: Offset(0.5, 2))
+                                ]),
+                          )),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: genratestar(_list[index].noOfstar),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  List<Widget> genratestar(int no) {
+    List<Widget> _icons = [];
+    for (int i = 0; i < no; i++) {
+      _icons.insert(
+          i,
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ));
+    }
+    return _icons;
+  }
+}
+
+class Details {
+  final String name;
+  final Color primarycolor;
+  final Color secomdarycolor;
+  final Widget goto;
+  final int noOfstar;
+
+  Details(
+      {required this.name,
+      required this.primarycolor,
+      required this.secomdarycolor,
+      required this.noOfstar,
+      required this.goto});
+}
+
+List<Details> _list = [
+  Details(
+      name: "FÁCIL",
+      primarycolor: Colors.green,
+      secomdarycolor: Colors.green[300]!,
+      noOfstar: 1,
+      goto: FlipCardGane(Level.Easy)),
+  Details(
+      name: "MEDIO",
+      primarycolor: Colors.orange,
+      secomdarycolor: Colors.orange[300]!,
+      noOfstar: 2,
+      goto: FlipCardGane(Level.Medium)),
+    Details(
+      name: "DIFICÍL",
+      primarycolor: Colors.red,
+      secomdarycolor: Colors.red[300]!,
+      noOfstar: 3,
+      goto: FlipCardGane(Level.Hard))
+];
